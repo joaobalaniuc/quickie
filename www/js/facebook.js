16c4cf3e4680ca279fec03ab3e454bde3fa187c6
João Williams Balaniúc <jwillbala@hotmail.com>
2016-05-12 18:15:13 -0300

    master

--- www/js/facebook.js
+++ www/js/facebook.js
@@ -5,8 +5,6 @@
 var fb = {
     login: function () {
         facebookConnectPlugin.login(["email", "public_profile"], function (response) {
-            me.logged_in = true;
-            //alert('logged in successfully');
             alert(JSON.stringify(response));
             localStorage.fb_id = response.authResponse.userID;
             localStorage.fb_token = response.authResponse.accessToken;
